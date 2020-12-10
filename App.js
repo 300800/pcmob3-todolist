2c446de0764e510407df42e823af34d23b42ed99
grace <gracechongbl@gmail.com>
2020-12-10 23:22:22 +0800

    added stacknavigator, ionicons, etc

--- App.js
+++ App.js
@@ -1,33 +1,71 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Ionicons } from "@expo/vector-icons";
+import * as SQLite from "expo-sqlite";
 
-function NotesScreen() {
+const db = SQLite.openDatabase("notes.db");
+
+function NotesScreen({ navigation }) {
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={addNote}>
+          <Ionicons
+            name="ios-create-outline"
+            size={30}
+            color="black"
+            style={{ marginRight: 20 }}
+          />
+        </TouchableOpacity>
+      ),
+    });
+  });
+
+  function addNote() {
+    console.log("Add note");
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on yur App!</Text>
-      <statusBar style="auto" />
+      <StatusBar style="auto" />
     </View>
   );
 }
 
-const stack = createStackNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Notes"
+          component={NotesScreen}
+          options={{
+            headerTitle: "Notes, a Todo App",
+            headerTitleStyle: {
+              backgroundColor: "yellow",
+            },
+            headerTintColor: "#f55",
+            headerTitleStyle: {
+              fontSize: 24,
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#ffc",
     alignItems: "center",
     justifyContent: "center",
   },
