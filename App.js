0bb45cd530da4f2e6674011a0c49d91b79c83461
grace <gracechongbl@gmail.com>
2021-03-07 18:47:32 +0800

    set up Add Screen, Note Screen and Note Stack, set up Navigation, SQLite database, execute SQL, create stack Navigator, etc

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { FlatList, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import * as SQLite from "expo-sqlite";
+import AddScreen from "./screens/AddScreen";
+import NotesStack from "./screens/NotesStack";
 
 const db = SQLite.openDatabase("notes.db");
 
+const SAMPLE_NOTES = [
+  { title: "Water the garden", done: false, id: "0" },
+  { title: "Feed the dogs", done: false, id: "1" },
+  { title: "More sample data", done: false, id: "2" },
+];
+
 function NotesScreen({ navigation }) {
+  const [notes, setNotes] = useState(SAMPLE_NOTES);
+
   useEffect(() => {
     navigation.setOptions({
       headerRight: () => (
@@ -26,13 +36,32 @@ function NotesScreen({ navigation }) {
   });
 
   function addNote() {
-    console.log("Add note");
+    navigation.navigate("Add Screen");
+  }
+
+  function renderItem({ item }) {
+    return (
+      <View
+        style={{
+          padding: 10,
+          paddingTop: 20,
+          paddingBottom: 20,
+          borderBottomColor: "#ccc",
+          borderBottomWidth: 1,
+        }}
+      >
+        <Text>{item.title}</Text>
+      </View>
+    );
   }
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on yur App!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={SAMPLE_NOTES}
+        renderItem={renderItem}
+        style={{ width: "100%" }}
+      />
     </View>
   );
 }
@@ -42,21 +71,9 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Notes"
-          component={NotesScreen}
-          options={{
-            headerTitle: "Notes, a Todo App",
-            headerTitleStyle: {
-              backgroundColor: "yellow",
-            },
-            headerTintColor: "#f55",
-            headerTitleStyle: {
-              fontSize: 24,
-            },
-          }}
-        />
+      <Stack.Navigator headerMode="none" mode="modal">
+        <Stack.Screen name="Notes Stack" component={NotesStack} />
+        <Stack.Screen name="Add Screen" component={AddScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
